8009f613be45324971c5da62f835db3db54b41bf
Sridhar Kumaresan <sridharkumaresan@gmail.com>
2021-03-30 12:22:47 -0400

    Countdown added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interview-prab';
+  start() {
+    console.log('Start Test');
+  }
+  handleEvent(event: any) {
+    console.log('event', event);
+  }
 }
